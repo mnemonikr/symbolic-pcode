e6c3132a5e1af718cd60ec057f9b5c6de62f156f
mnemonikr <138624285+mnemonikr@users.noreply.github.com>
2024-02-09 16:16:43 -0800

    Clean up some of the benchmark code

--- benches/emulator.rs
+++ benches/emulator.rs
@@ -1,8 +1,9 @@
 use std::borrow::Cow;
 
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
 use sla::{
-    Address, AddressSpace, AddressSpaceType, IntOp, IntSign, OpCode, PcodeInstruction, VarnodeData,
+    Address, AddressSpace, AddressSpaceType, BoolOp, IntOp, IntSign, OpCode, PcodeInstruction,
+    VarnodeData,
 };
 use sym::SymbolicByte;
 use symbolic_pcode::emulator::{PcodeEmulator, StandardPcodeEmulator};
@@ -196,288 +197,141 @@ fn setup_store() -> (Memory, PcodeInstruction) {
     (memory, instruction)
 }
 
-fn setup_int_sub() -> (Memory, PcodeInstruction) {
-    let mut memory = Memory::new();
-    let instruction = PcodeInstruction {
-        address: Address {
-            address_space: processor_space(),
-            offset: 0xFF00000000,
-        },
-        op_code: OpCode::Int(IntOp::Subtract),
-        inputs: vec![
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 0,
+fn create_arithmetic_setup_fn(op_code: OpCode) -> impl FnMut() -> (Memory, PcodeInstruction) {
+    move || {
+        let input_size = match op_code {
+            OpCode::Bool(_) => 1,
+            _ => 8,
+        };
+
+        let inputs = match op_code {
+            OpCode::Bool(BoolOp::Negate) | OpCode::Int(IntOp::Bitwise(BoolOp::Negate)) => {
+                vec![VarnodeData {
+                    address: Address {
+                        address_space: internal_space(),
+                        offset: 0,
+                    },
+                    size: input_size,
+                }]
+            }
+            _ => vec![
+                VarnodeData {
+                    address: Address {
+                        address_space: internal_space(),
+                        offset: 0,
+                    },
+                    size: input_size,
                 },
-                size: 8,
-            },
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 8,
+                VarnodeData {
+                    address: Address {
+                        address_space: internal_space(),
+                        offset: 8,
+                    },
+                    size: input_size,
                 },
-                size: 8,
-            },
-        ],
-        output: Some(VarnodeData {
+            ],
+        };
+
+        let output_size = match op_code {
+            OpCode::Int(IntOp::Carry(_) | IntOp::Borrow) => 1,
+            OpCode::Int(
+                IntOp::Equal | IntOp::NotEqual | IntOp::LessThan(_) | IntOp::LessThanOrEqual(_),
+            ) => 1,
+            OpCode::Bool(_) => 1,
+            _ => 8,
+        };
+
+        let mut memory = Memory::new();
+        let instruction = PcodeInstruction {
             address: Address {
-                address_space: internal_space(),
-                offset: 16,
+                address_space: processor_space(),
+                offset: 0xFF00000000,
             },
-            size: 8,
-        }),
-    };
-
-    memory
-        .write(
-            &instruction.inputs[0],
-            0xfedcba9876543210u64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write lhs");
-
-    memory
-        .write(
-            &instruction.inputs[1],
-            0x0123456789abcdefu64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write rhs");
-
-    (memory, instruction)
-}
-
-fn setup_int_sub_borrow() -> (Memory, PcodeInstruction) {
-    let mut memory = Memory::new();
-    let instruction = PcodeInstruction {
-        address: Address {
-            address_space: processor_space(),
-            offset: 0xFF00000000,
-        },
-        op_code: OpCode::Int(IntOp::Borrow),
-        inputs: vec![
-            VarnodeData {
+            op_code,
+            inputs,
+            output: Some(VarnodeData {
                 address: Address {
                     address_space: internal_space(),
-                    offset: 0,
+                    offset: 16,
                 },
-                size: 8,
-            },
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 8,
-                },
-                size: 8,
-            },
-        ],
-        output: Some(VarnodeData {
-            address: Address {
-                address_space: internal_space(),
-                offset: 16,
-            },
-            size: 1,
-        }),
-    };
+                size: output_size,
+            }),
+        };
 
-    memory
-        .write(
-            &instruction.inputs[0],
-            0xfedcba9876543210u64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write lhs");
+        memory
+            .write(
+                &instruction.inputs[0],
+                0xfedcba9876543210u64
+                    .to_le_bytes()
+                    .into_iter()
+                    .map(SymbolicByte::from)
+                    .take(instruction.inputs[0].size),
+            )
+            .expect("failed to write lhs");
 
-    memory
-        .write(
-            &instruction.inputs[1],
-            0x0123456789abcdefu64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write rhs");
+        if instruction.inputs.len() > 1 {
+            memory
+                .write(
+                    &instruction.inputs[1],
+                    0x0123456789abcdefu64
+                        .to_le_bytes()
+                        .into_iter()
+                        .map(SymbolicByte::from)
+                        .take(instruction.inputs[1].size),
+                )
+                .expect("failed to write rhs");
+        }
 
-    (memory, instruction)
-}
-
-fn setup_int_multiply() -> (Memory, PcodeInstruction) {
-    let mut memory = Memory::new();
-    let instruction = PcodeInstruction {
-        address: Address {
-            address_space: processor_space(),
-            offset: 0xFF00000000,
-        },
-        op_code: OpCode::Int(IntOp::Multiply),
-        inputs: vec![
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 0,
-                },
-                size: 8,
-            },
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 8,
-                },
-                size: 8,
-            },
-        ],
-        output: Some(VarnodeData {
-            address: Address {
-                address_space: internal_space(),
-                offset: 16,
-            },
-            size: 8,
-        }),
-    };
-
-    memory
-        .write(
-            &instruction.inputs[0],
-            0xfedcba9876543210u64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write lhs");
-
-    memory
-        .write(
-            &instruction.inputs[1],
-            0x0123456789abcdefu64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write rhs");
-
-    (memory, instruction)
-}
-
-fn setup_int_divide_unsigned() -> (Memory, PcodeInstruction) {
-    let mut memory = Memory::new();
-    let instruction = PcodeInstruction {
-        address: Address {
-            address_space: processor_space(),
-            offset: 0xFF00000000,
-        },
-        op_code: OpCode::Int(IntOp::Divide(IntSign::Unsigned)),
-        inputs: vec![
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 0,
-                },
-                size: 8,
-            },
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 8,
-                },
-                size: 8,
-            },
-        ],
-        output: Some(VarnodeData {
-            address: Address {
-                address_space: internal_space(),
-                offset: 16,
-            },
-            size: 8,
-        }),
-    };
-
-    memory
-        .write(
-            &instruction.inputs[0],
-            0xfedcba9876543210u64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write lhs");
-
-    memory
-        .write(
-            &instruction.inputs[1],
-            0x0123456789abcdefu64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write rhs");
-
-    (memory, instruction)
-}
-
-fn setup_int_divide_signed() -> (Memory, PcodeInstruction) {
-    let mut memory = Memory::new();
-    let instruction = PcodeInstruction {
-        address: Address {
-            address_space: processor_space(),
-            offset: 0xFF00000000,
-        },
-        op_code: OpCode::Int(IntOp::Divide(IntSign::Signed)),
-        inputs: vec![
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 0,
-                },
-                size: 8,
-            },
-            VarnodeData {
-                address: Address {
-                    address_space: internal_space(),
-                    offset: 8,
-                },
-                size: 8,
-            },
-        ],
-        output: Some(VarnodeData {
-            address: Address {
-                address_space: internal_space(),
-                offset: 16,
-            },
-            size: 8,
-        }),
-    };
-
-    memory
-        .write(
-            &instruction.inputs[0],
-            0xfedcba9876543210u64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write lhs");
-
-    memory
-        .write(
-            &instruction.inputs[1],
-            0x0123456789abcdefu64
-                .to_le_bytes()
-                .into_iter()
-                .map(SymbolicByte::from),
-        )
-        .expect("failed to write rhs");
-
-    (memory, instruction)
+        (memory, instruction)
+    }
 }
 
 pub fn standard_emulator(c: &mut Criterion) {
     let emulator = StandardPcodeEmulator::new([internal_space(), processor_space()]);
+
+    let mut bench_arithmetic = |op_code| {
+        let mut group = c.benchmark_group("Emulate");
+        group.bench_function(
+            BenchmarkId::new("Arithmetic", format!("{op_code:?}")),
+            |b| {
+                b.iter_batched(
+                    create_arithmetic_setup_fn(op_code),
+                    |mut data| {
+                        emulator
+                            .emulate(&mut data.0, &data.1)
+                            .expect("failed to emulate instruction")
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+        group.finish();
+    };
+
+    bench_arithmetic(OpCode::Int(IntOp::Subtract));
+    bench_arithmetic(OpCode::Int(IntOp::Borrow));
+    bench_arithmetic(OpCode::Int(IntOp::Multiply));
+    bench_arithmetic(OpCode::Int(IntOp::Divide(IntSign::Unsigned)));
+    bench_arithmetic(OpCode::Int(IntOp::Divide(IntSign::Signed)));
+    bench_arithmetic(OpCode::Int(IntOp::Remainder(IntSign::Unsigned)));
+    bench_arithmetic(OpCode::Int(IntOp::Remainder(IntSign::Signed)));
+    bench_arithmetic(OpCode::Int(IntOp::Add));
+    bench_arithmetic(OpCode::Int(IntOp::Carry(IntSign::Unsigned)));
+    bench_arithmetic(OpCode::Int(IntOp::Carry(IntSign::Signed)));
+    bench_arithmetic(OpCode::Int(IntOp::Equal));
+    bench_arithmetic(OpCode::Int(IntOp::NotEqual));
+    bench_arithmetic(OpCode::Int(IntOp::LessThan(IntSign::Unsigned)));
+    bench_arithmetic(OpCode::Int(IntOp::LessThan(IntSign::Signed)));
+    bench_arithmetic(OpCode::Int(IntOp::LessThanOrEqual(IntSign::Unsigned)));
+    bench_arithmetic(OpCode::Int(IntOp::LessThanOrEqual(IntSign::Signed)));
+    bench_arithmetic(OpCode::Int(IntOp::Bitwise(BoolOp::And)));
+    bench_arithmetic(OpCode::Int(IntOp::Bitwise(BoolOp::Or)));
+    bench_arithmetic(OpCode::Int(IntOp::Bitwise(BoolOp::Xor)));
+    bench_arithmetic(OpCode::Int(IntOp::Bitwise(BoolOp::Negate)));
+    bench_arithmetic(OpCode::Bool(BoolOp::And));
+    bench_arithmetic(OpCode::Bool(BoolOp::Or));
+    bench_arithmetic(OpCode::Bool(BoolOp::Xor));
+    bench_arithmetic(OpCode::Bool(BoolOp::Negate));
 
     c.bench_function("copy", |b| {
         b.iter_batched(
@@ -506,66 +360,6 @@ pub fn standard_emulator(c: &mut Criterion) {
     c.bench_function("store", |b| {
         b.iter_batched(
             setup_store,
-            |mut data| {
-                emulator
-                    .emulate(&mut data.0, &data.1)
-                    .expect("failed to emulate instruction")
-            },
-            BatchSize::SmallInput,
-        )
-    });
-
-    c.bench_function("int_sub", |b| {
-        b.iter_batched(
-            setup_int_sub,
-            |mut data| {
-                emulator
-                    .emulate(&mut data.0, &data.1)
-                    .expect("failed to emulate instruction")
-            },
-            BatchSize::SmallInput,
-        )
-    });
-
-    c.bench_function("int_sub_borrow", |b| {
-        b.iter_batched(
-            setup_int_sub_borrow,
-            |mut data| {
-                emulator
-                    .emulate(&mut data.0, &data.1)
-                    .expect("failed to emulate instruction")
-            },
-            BatchSize::SmallInput,
-        )
-    });
-
-    c.bench_function("int_multiply", |b| {
-        b.iter_batched(
-            setup_int_multiply,
-            |mut data| {
-                emulator
-                    .emulate(&mut data.0, &data.1)
-                    .expect("failed to emulate instruction")
-            },
-            BatchSize::SmallInput,
-        )
-    });
-
-    c.bench_function("int_divide_unsigned", |b| {
-        b.iter_batched(
-            setup_int_divide_unsigned,
-            |mut data| {
-                emulator
-                    .emulate(&mut data.0, &data.1)
-                    .expect("failed to emulate instruction")
-            },
-            BatchSize::SmallInput,
-        )
-    });
-
-    c.bench_function("int_divide_signed", |b| {
-        b.iter_batched(
-            setup_int_divide_signed,
             |mut data| {
                 emulator
                     .emulate(&mut data.0, &data.1)
