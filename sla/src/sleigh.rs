85453aaff6ac32e1f34978cda9c859d7cdd879fd
mnemonikr <138624285+mnemonikr@users.noreply.github.com>
2024-03-01 23:22:57 -0800

    Rename INIT to LIBSLA_INIT (#128)

--- sla/src/sleigh.rs
+++ sla/src/sleigh.rs
@@ -10,7 +10,8 @@ use crate::ffi::sys;
 use crate::ffi::sys::AddrSpace;
 use crate::opcodes::OpCode;
 
-static INIT: Once = Once::new();
+/// Tracks whether the one-time initialization required for libsla has been performed
+static LIBSLA_INIT: Once = Once::new();
 
 /// Errors returned by this crate. Note that some APIs that may pass through FFI boundaries return
 /// [String] since those errors are ultimately serialized anyway.
@@ -416,7 +417,7 @@ impl GhidraSleigh {
 
     pub fn initialize(&mut self, sleigh_spec: &str, processor_spec: &str) -> Result<()> {
         // This global libsla initialization is required for parsing sleigh document
-        INIT.call_once(|| {
+        LIBSLA_INIT.call_once(|| {
             sys::initialize_element_id();
             sys::initialize_attribute_id();
         });
